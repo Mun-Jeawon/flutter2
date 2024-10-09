17dfe7afe7041b5907b8ff9314193538050ec3e7
2Daehan <eogks324@gmail.com>
2024-10-09 20:11:05 +0900

    2

--- lib/main.dart
+++ lib/main.dart
@@ -31,48 +31,52 @@ class _CalendarPageState extends State<CalendarPage> {
         backgroundColor: Colors.black,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          TableCalendar(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: _focusedDay,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay; // update `_focusedDay` here as well
-              });
-            },
-            calendarFormat: CalendarFormat.month,
-            headerStyle: HeaderStyle(
-              formatButtonVisible: false,
-              titleCentered: true,
-            ),
-            calendarStyle: CalendarStyle(
-              todayDecoration: BoxDecoration(
-                color: Colors.black12,
-                shape: BoxShape.circle,
+          // Container로 감싸서 여백 조정
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 300), // 양쪽 여백 조정
+            child: TableCalendar(
+              firstDay: DateTime.utc(2020, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay; // update `_focusedDay` here as well
+                });
+              },
+              calendarFormat: CalendarFormat.month,
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
               ),
-              selectedDecoration: BoxDecoration(
-                color: Colors.black,
-                shape: BoxShape.circle,
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Colors.black12,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.circle,
+                ),
+                outsideDaysVisible: false,
               ),
-              outsideDaysVisible: false,
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 10),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
             child: Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     _buildButton(Icons.check_box, '체크리스트'),
-                    SizedBox(width: 20), // Add spacing between buttons
+                    SizedBox(width: 20), // 버튼 간의 간격 추가
                     _buildButton(Icons.notifications, '알람'),
                   ],
                 ),
@@ -81,7 +85,7 @@ class _CalendarPageState extends State<CalendarPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     _buildButton(Icons.link, '영양제'),
-                    SizedBox(width: 20), // Add spacing between buttons
+                    SizedBox(width: 20), // 버튼 간의 간격 추가
                     _buildButton(Icons.bar_chart, '통계'),
                   ],
                 ),
@@ -95,17 +99,17 @@ class _CalendarPageState extends State<CalendarPage> {
 
   Widget _buildButton(IconData icon, String label) {
     return Container(
-      width: 120, // Fixed width
-      height: 60, // Fixed height
+      width: 120, // 고정 너비
+      height: 60, // 고정 높이
       child: ElevatedButton.icon(
         onPressed: () {
-          // 버튼 클릭시 동작할 로직을 여기에 추가
+          // 버튼 클릭 시 동작할 로직 추가
         },
         icon: Icon(icon, color: Colors.white),
         label: Text(label, style: TextStyle(color: Colors.white)),
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.black,
-          padding: EdgeInsets.zero, // Reset padding to use fixed dimensions
+          padding: EdgeInsets.zero, // 패딩 초기화
         ),
       ),
     );
